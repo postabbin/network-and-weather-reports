c85d907735e4864a84d7181e7981db84f313a793
Abbin Varghese <42763587+postabbin@users.noreply.github.com>
2019-12-20 11:25:00 +0530

    Comm

---
+++ Dashboard/Dashboard/Dashboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dashboard
+//
+//  Created by Abbin Varghese on 20/12/19.
+//  Copyright © 2019 Abbin Varghese. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
